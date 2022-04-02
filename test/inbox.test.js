be8c030260fbb86bff3f0132f89b494dfab67e8e
Grammed <grahamd483@gmail.com>
2022-04-02 19:35:24 +0200

    closes #4 async/await

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,23 +3,13 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-/* Mocha: it -> run a test and make an assertion
- describe -> groups together it funcs
- beforeEach -> Execute general setUp code
-*/
+let accounts;
+beforeEach(async () => {
+   accounts = await web3.eth.getAccounts()
+});
 
-beforeEach(() => {
-    // get list of all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts)
-        })
-    // use an account to deploy a contract.
-})
-
-// Added only to run beforeEach
 describe('Inbox', () => {
     it('deploys a contract', () => {
-
+        console.log(accounts);
     })
-})
+});
