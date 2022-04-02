2e8a2a37b0a5cbffa63f01c594f6bd530f080d12
Grammed <grahamd483@gmail.com>
2022-04-02 22:11:28 +0200

    closes #5 contract deployed in test env

--- compile.js
+++ compile.js
@@ -1,14 +1,16 @@
-"use strict";
-
+const solc = require('solc')
 const path = require('path');
 const fs = require('fs');
+// node compile.js
+
 
 // here __dirname resolves to BoilerplateSolidity
 const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = compile(source, 1).contracts[':Inbox'];
 
+//console.log(solc.compile(source, 1).contracts[':inbox']);
+module.exports  = solc.compile(source, 1).contracts[':inbox'];
 
 
 
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,14 +2,22 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+// js ABI and contract bytecode: Whereas an API defines a source interface, an ABI defines the low-level binary interface.
+const {interface:aBI, bytecode} = require('../compile');
 
 let accounts;
+let inbox;
 beforeEach(async () => {
    accounts = await web3.eth.getAccounts()
+
+    // use an account to deploy the contract:
+    inbox = await new  web3.eth.Contract(JSON.parse(aBI))
+        .deploy({data: bytecode, arguments: ['Well hello there..!']})
+        .send({from: accounts[0], gas: 1_000_000});
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     })
 });
